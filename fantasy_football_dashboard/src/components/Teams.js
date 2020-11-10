712b5a3143aa1a24118f9aa7f9c84fb19abd257b
joeypowers <jjpowers@purdue.edu>
2020-11-09 21:00:59 -0500

    Added multiple rosters

--- fantasy_football_dashboard/src/components/Teams.js
+++ fantasy_football_dashboard/src/components/Teams.js
@@ -9,17 +9,185 @@ class Teams extends Component {
           {
             id: 1,
             name: "Red Solo Kupp",
-            platform: "NFL"
+            platform: "NFL",
+            team_roster: [
+              {
+                pos: "QB",
+                roster_slot: "QB",
+                name: "Joe Burrow",
+                team: "Bengals"
+              },
+              {
+                pos: "RB",
+                roster_slot: "RB",
+                name: "Ezekiel Elliot",
+                team: "Cowboys"
+              },
+              {
+                pos: "RB",
+                roster_slot: "RB",
+                name: "Josh Jacobs",
+                team: "Raiders"
+              },
+              {
+                  pos: "WR",
+                  roster_slot: "WR",
+                  name: "Terry Mclaurin",
+                  team: "Football Team"
+                },
+                {
+                  pos: "WR",
+                  roster_slot: "WR",
+                  name: "Julio Jones",
+                  team: "Falcons"
+                },
+                {
+                  pos: "TE",
+                  roster_slot: "TE",
+                  name: "George Kittle",
+                  team: "49ers"
+                },
+                {
+                  pos: "RB",
+                  roster_slot: "W/R",
+                  name: "Miles Sanders",
+                  team: "Eagles"
+                },
+                {
+                  pos: "WR",
+                  roster_slot: "W/R",
+                  name: "CeeDee Lamb",
+                  team: "Cowboys"
+                },
+                {
+                  pos: "K",
+                  roster_slot: "K",
+                  name: "Younghoe Koo",
+                  team: "Falcons"
+                },
+                {
+                  pos: "DEF",
+                  roster_slot: "DEF",
+                  name: "Vikings",
+                  team: "Vikings"
+                }
+            ]
           },
           {
             id: 2,
             name: "Hooked on a Thielen",
-            platform: "ESPN"
+            platform: "ESPN",
+            team_roster: [
+              {
+                pos: "QB",
+                roster_slot: "QB",
+                name: "Matt Ryan",
+                team: "Falcons"
+                },
+                {
+                  pos: "RB",
+                  roster_slot: "RB",
+                  name: "Derrick Henry",
+                  team: "Titans"
+                },
+                {
+                  pos: "RB",
+                  roster_slot: "RB",
+                  name: "Kareem Hunt",
+                  team: "Browns"
+                },
+                {
+                  pos: "WR",
+                  roster_slot: "WR",
+                  name: "Julio Jones",
+                  team: "Falcons"
+                },
+                {
+                  pos: "WR",
+                  roster_slot: "WR",
+                  name: "Adam Thielen",
+                  team: "Vikings"
+                },
+                {
+                  pos: "TE",
+                  roster_slot: "TE",
+                  name: "Austin Hooper",
+                  team: "Browns"
+                },
+                {
+                  pos: "WR",
+                  roster_slot: "W/R",
+                  name: "DK Metcalf",
+                  team: "Seahawks"
+                },
+                {
+                  pos: "K",
+                  roster_slot: "K",
+                  name: "Daniel Carlson",
+                  team: "Raiders"
+                },
+                {
+                  pos: "DEF",
+                  roster_slot: "DEF",
+                  name: "Chiefs",
+                  team: "Chiefs"
+                }
+            ]
           },
           {
             id: 3,
             name: "Scary Terry in the Kitchen",
-            platform: "Sleeper"
+            platform: "Sleeper",
+            team_roster: [
+              {
+                pos: "QB",
+                roster_slot: "QB",
+                name: "Jimmy Garropolo",
+                team: "49ers"
+              },
+              {
+                pos: "RB",
+                roster_slot: "RB",
+                name: "James Conner",
+                team: "Steelers"
+              },
+              {
+                pos: "RB",
+                roster_slot: "RB",
+                name: "Dalvin Cook",
+                team: "Vikings"
+              },
+              {
+                pos: "WR",
+                roster_slot: "WR",
+                name: "Mike Evans",
+                team: "Bucaneers"
+              },
+              {
+                pos: "WR",
+                roster_slot: "WR",
+                name: "Terry Mclaurin",
+                team: "Football Team"
+              },
+              {
+                pos: "TE",
+                roster_slot: "TE",
+                name: "Mo Alie Cox",
+                team: "Colts"
+              },
+              {
+                pos: "RB",
+                roster_slot: "W/R",
+                name: "Jonathan Taylor",
+                team: "Colts"
+              },
+              {
+                pos: "QB",
+                roster_slot: "WRTQ",
+                name: "Deshaun Watson",
+                team: "Texans"
+              }
+            ]
           },
         ]
       }
--- fantasy_football_dashboard/src/components/pages/TeamView.js
+++ fantasy_football_dashboard/src/components/pages/TeamView.js
@@ -2,77 +2,14 @@ import { render } from '@testing-library/react'
 import React, { Component } from 'react'
 import Roster from './Roster';
 import { Link } from 'react-router-dom';
+import Teams from '../Teams.js'
 
 class TeamView extends Component{
-    state = {
-        team_roster: [
-            {
-              pos: "QB",
-              roster_slot: "QB",
-              name: "Joe Burrow",
-              team: "Bengals"
-            },
-            {
-              pos: "RB",
-              roster_slot: "RB",
-              name: "Ezekiel Elliot",
-              team: "Cowboys"
-            },
-            {
-              pos: "RB",
-              roster_slot: "RB",
-              name: "Josh Jacobs",
-              team: "Raiders"
-            },
-            {
-                pos: "WR",
-                roster_slot: "WR",
-                name: "Terry Mclaurin",
-                team: "Football Team"
-              },
-              {
-                pos: "WR",
-                roster_slot: "WR",
-                name: "Julio Jones",
-                team: "Falcons"
-              },
-              {
-                pos: "TE",
-                roster_slot: "TE",
-                name: "George Kittle",
-                team: "49ers"
-              },
-              {
-                pos: "RB",
-                roster_slot: "W/R",
-                name: "Miles Sanders",
-                team: "Eagles"
-              },
-              {
-                pos: "WR",
-                roster_slot: "W/R",
-                name: "CeeDee Lamb",
-                team: "Cowboys"
-              },
-              {
-                pos: "K",
-                roster_slot: "K",
-                name: "Younghoe Koo",
-                team: "Falcons"
-              },
-              {
-                pos: "DEF",
-                roster_slot: "DEF",
-                name: "Vikings",
-                team: "Vikings"
-              }
-
-          ]
-    }
+   
     render() {
       return (
         <div className="App">
-          <Roster roster={this.state.team_roster}/>
+          <Roster roster={this.Teams.state.teams.roster}/>
           <Link to="/">
             <p>Return to Home</p>  
           </Link>
