29ee615db56178b9f8e3e8c1124defb499ec4054
joeypowers <jjpowers@purdue.edu>
2020-11-17 20:43:25 -0500

    Added submit button and email field to create account

--- fantasy_football_dashboard/src/components/pages/CreateAccount.js
+++ fantasy_football_dashboard/src/components/pages/CreateAccount.js
@@ -7,14 +7,22 @@ export default class CreateAccount extends Component {
                 <h1>Create Account:</h1>
                 <form>
                     <label>
+                        <h4>Email: </h4>
+                        <input type="email" placeholder="Enter Username" name="email" style={{ width:'30%'}} required />
+                    </label>
+                    <label>
                         <h4>Username: </h4>
                         <input type="text" placeholder="Enter Username" name="username" style={{ width:'30%' }} required />
                     </label>
                     <label>
                         <h4>Password: </h4>
-                        <input type="password" placeholder="Enter Password" name="password" style={{ width: '30%' }} required />
+                        <input type="password" placeholder="Enter Password" name="password" style={{ width:'30%' }} required />
                     </label>
+                    <br/>
+                    <br/>
+                    <input type="submit" value="Submit" />
                 </form>
+                
                 
             </div>
         )
